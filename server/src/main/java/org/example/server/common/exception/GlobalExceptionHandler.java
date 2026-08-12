ff4fe3540d67bab0e3550558d5855120a084f394
Kangila404 <ia3264666@gmail.com>
2026-08-12 12:06:08 +0900

    fix(server) : 글로벌 핸들러 상세 에러 반환 메서드 추가, 코드 레빗 yml 추가

--- server/src/main/java/org/example/server/common/exception/GlobalExceptionHandler.java
+++ server/src/main/java/org/example/server/common/exception/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package org.example.server.common.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -20,5 +22,12 @@ public class GlobalExceptionHandler {
         return ResponseEntity
             .status(HttpStatus.INTERNAL_SERVER_ERROR)
             .body(ErrorResponse.of(CommonErrorCode.INTERNAL_SERVER_ERROR));
+    }
+
+    @ExceptionHandler({HttpMessageNotReadableException.class, MethodArgumentNotValidException.class})
+    public ResponseEntity<ErrorResponse> handleBadRequest(Exception e) {
+        return ResponseEntity
+            .status(CommonErrorCode.INVALID_REQUEST.getStatus())
+            .body(ErrorResponse.of(CommonErrorCode.INVALID_REQUEST));
     }
 }
