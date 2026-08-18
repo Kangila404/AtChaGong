b75035b5a28b2323231dbbe8f82e9e518d9c286b
gmlwlsdl <heejin094@naver.com>
2026-08-18 23:50:17 +0900

    feat(server): 사용자 알림 설정 삭제 추가

--- server/src/main/java/org/example/server/notification/infrastructure/persistence/repository/NotificationSettingJpaRepository.java
+++ server/src/main/java/org/example/server/notification/infrastructure/persistence/repository/NotificationSettingJpaRepository.java
@@ -3,7 +3,13 @@ package org.example.server.notification.infrastructure.persistence.repository;
 import java.util.Optional;
 import org.example.server.notification.domain.models.NotificationSetting;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface NotificationSettingJpaRepository extends JpaRepository<NotificationSetting, Long> {
     Optional<NotificationSetting> findByUserId(Long userId);
+
+    @Modifying
+    @Query("delete from NotificationSetting notificationSetting where notificationSetting.userId = :userId")
+    void deleteByUserId(Long userId);
 }
