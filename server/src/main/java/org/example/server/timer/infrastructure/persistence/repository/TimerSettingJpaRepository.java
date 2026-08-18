3388fe1e14de8849e0c88119ca7cf0c80bf4ea44
gmlwlsdl <heejin094@naver.com>
2026-08-18 23:49:58 +0900

    feat(server): 사용자 타이머 설정 삭제 추가

--- server/src/main/java/org/example/server/timer/infrastructure/persistence/repository/TimerSettingJpaRepository.java
+++ server/src/main/java/org/example/server/timer/infrastructure/persistence/repository/TimerSettingJpaRepository.java
@@ -3,8 +3,14 @@ package org.example.server.timer.infrastructure.persistence.repository;
 import java.util.Optional;
 import org.example.server.timer.domain.models.TimerSetting;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface TimerSettingJpaRepository extends JpaRepository<TimerSetting,Long> {
 
     Optional<TimerSetting> findByUserId(Long userId);
+
+    @Modifying
+    @Query("delete from TimerSetting timerSetting where timerSetting.userId = :userId")
+    void deleteByUserId(Long userId);
 }
